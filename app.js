92c2379e9f064f6996460bcb2ea75413e91e9475
unknown <jotpal_07@yahoo.com>
2017-09-03 14:10:14 +0530

    updates

--- app.js
+++ app.js
@@ -168,7 +168,7 @@ app.get('/patient/:_id/:queueId',(req,res)=>{
     } 
 });
 
-app.get('/queuebuilder',(req,res)=>{
+app.get('/queuebuilder', isLoggedIn,(req,res)=>{
     Queue.getQueues((err, queues) =>{
         if(err){
             throw err;
@@ -278,6 +278,18 @@ app.post('/login', passport.authenticate('local-login', {
     failureFlash : true // allow flash messages
 }));
 // start server
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on 
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
+
 app.listen(port, () => {
     console.log('Server running on port :'+port);
 });
